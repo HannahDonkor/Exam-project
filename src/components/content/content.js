8823c0b97f6c431c727b14abf1bcedce1c634f3f
HannahDonkor <108756205+HannahDonkor@users.noreply.github.com>
2022-11-06 23:12:21 +0000

    Add files via upload

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -1,62 +1,23 @@
 import React, {useState} from 'react'
 import './content.css';
+import useCounter from '../../hooks/useCounter';
 
 const Content = () => {
-const [count, setCount] = useState(0)
-const [show, setShow] = useState(false)
-const [customValue, setCustomValue] = useState()
-
-
-//increases value by 1
-    function increaseCount(){
-        setCount(prevValue => prevValue +1)
-    
-    }
-
-//decreases value by 1
-    function decreaseCount(){
-        if (count===0){
-            return
-        }
-        setCount(prevValue => prevValue -1)
-
-    }
-
-//sets to default value
-    function resetCount(){
-        setCount(prevValue => prevValue *0)
-        
-    }
-
-//shows the pop-up
-    function setValueCount(){
-        setShow (true)
-    }
-
-//to input desired figures for custom value
-    function handleInput(event){
-        setCustomValue (event.target.value)
-    }
-
- //set custom value to counter for pop-up to disappear   
-    function handleCustomValue (){
-        setCount (customValue)
-        setShow (false)
-    }
+    const[count, increaseCount, decreaseCount, resetCount, setValueCount, show, handleInput,handleCustomValue] = useCounter()
 
 
   return (
-    <div>
+    <div className='main'>
         <div className='bio'>
-            <h2>The complete counter for things that matter to you.</h2>
+            {/* <h2>The complete counter for things that matter to you.</h2> */}
 
         </div>
         <div className='timer'>{count}</div>
         <div className='buttons-container'>
-            <button className='btn' onClick={increaseCount}>Increment</button>
-            <button className='btn'onClick={decreaseCount}>Decrement</button>
+            <button className='btn' onClick={increaseCount}>Increase</button>
+            <button className='btn'onClick={decreaseCount}>Decrease</button>
             <button className='btn'onClick={resetCount}>Reset</button>
-            <button className='btn'onClick={setValueCount}>setValue</button>
+            <button className='btn'onClick={setValueCount}>Set Score</button>
         </div>
         <div className={show ? 'active' : 'pop-up'}>
             <input onChange={handleInput}/>
